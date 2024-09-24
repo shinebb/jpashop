ff8b8d351779a960fdd919b060e466d4619c17d3
dohee <ododalrio@gmail.com>
2024-09-24 15:20:39 +0900

    240924 JPA와 DB설정

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+//Repository : Entity 찾아주는 역할, DAO랑 비슷하다고 생각하면 된다.
+@Repository
+public class MemberRepository {
+
+    //Test 단축키 (win) shift+ctrl+T
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}

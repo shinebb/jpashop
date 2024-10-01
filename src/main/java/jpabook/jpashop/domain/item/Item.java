cd20f3e15c5e5b9624ef6c4aaebfdd61e6b07e13
Dohee Kim <ododalrio@gmail.com>
2024-10-01 22:34:05 +0900

    241001 상품 서비스 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,12 +21,30 @@ public abstract class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
-    private int stockQuantity;
+    private int stockQuantity; //재고수량
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 재고 수량 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 재고 수량 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    //오버라이드 단축키 ctrl + O
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+}
